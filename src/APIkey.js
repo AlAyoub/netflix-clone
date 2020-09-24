53d845260419b41fbeedb042d2dbd01a68894982
Alan Ayoub <alanayoub@me.com>
2020-09-24 11:15:49 -0700

    rows are almost complete

---
+++ src/APIkey.js
@@ -0,0 +1,6 @@
+// My API Key from the TMDB database
+// aea53c6a7e005d22b663f88848aca936
+
+
+// Example API request
+// https://api.themoviedb.org/3/movie/550?api_key=aea53c6a7e005d22b663f88848aca936
